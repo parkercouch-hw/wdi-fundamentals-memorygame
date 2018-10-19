36187c7dce20b9afb4a385fe38308023d5f46191
Parker Couch <parkercouch@gmail.com>
2018-10-19 10:23:37 -0700

    basic game finished

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # wdi-fundamentals-memorygame
+
+Basic memory card game for wdi pre-work.
+
+TODO:
+* Make the match/not a match alert an element on the page instead of a popup
+* Add a "Reset" button to the HTML and set up an event in the JavaScript file so that the user can reset the game after playing.
+* Keep track of and display the user's score.
+* Add a larger board and randomize card placement
--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,4 @@
-console.log("Up and running!");
+//console.log("Up and running!");
 
 var cards = [
   {
@@ -24,6 +24,17 @@ var cards = [
 ];
 var cardsInPlay = [];
 
+
+var createBoard = function(){
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+  }
+}
+
 var checkForMatch = function(){
   if (cardsInPlay[0] === cardsInPlay[1]) {
     alert("You found a match!");
@@ -32,11 +43,14 @@ var checkForMatch = function(){
   }
 }
 
-var flipCard = function(cardId){
-  console.log("User flipped " + cards[cardId].rank);
-  console.log("Suit: " + cards[cardId].suit);
-  console.log("Image Path: " + cards[cardId].cardImage);
+var flipCard = function(){
+  var cardId = this.getAttribute('data-id');
+  //console.log("User flipped " + cards[cardId].rank);
+  //console.log("Suit: " + cards[cardId].suit);
+  //console.log("Image Path: " + cards[cardId].cardImage);
   cardsInPlay.push(cards[cardId].rank);
+
+  this.setAttribute('src', cards[cardId].cardImage);
 
   if (cardsInPlay.length === 2) {
     checkForMatch();
@@ -44,5 +58,4 @@ var flipCard = function(cardId){
 
 }
 
-flipCard(0);
-flipCard(2);
+createBoard();
