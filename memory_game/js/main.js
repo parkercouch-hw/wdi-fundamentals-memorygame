d9a40f4fc4677a0c808183c1d3f76c8c3a8aef11
Parker Couch <parkercouch@gmail.com>
2018-10-26 19:27:54 -0700

    feat: Add card generation and larger board
    This is the start of adding more features to the game. Started with
    a card generation function and a shuffle function so that a randomized
    board can be made. This will then turn into a game that can be reset and
    the size of the board customized by the user. There is a lot that needs
    changed before it can be considered production.

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,10 +1,15 @@
 //console.log("Up and running!");
 
-var cards = [
+var cardOptions = [
   {
     rank: "queen",
     suit: "hearts",
     cardImage: "images/queen-of-hearts.png"
+  },
+  {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png"
   },
   {
     rank: "queen",
@@ -13,23 +18,39 @@ var cards = [
   },
   {
     rank: "king",
-    suit: "hearts",
-    cardImage: "images/king-of-hearts.png"
-  },
-  {
-    rank: "king",
     suit: "diamonds",
     cardImage: "images/king-of-diamonds.png"
   }
 ];
+
+var cards = [];
+
 var cardsInPlay = [];
 var matchAlert = document.querySelector('.match-alert');
 
-var matchMessage = function (message) {
+var matchMessage = function(message) {
   matchAlert.innerHTML = message;
 };
 
-var createBoard = function(){
+//In place shuffle. Fisher-Yates Algorithm
+var shuffleCards = function(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+};
+
+var generateCards = function(sizeOfDeck, shuffle) {
+  // Function to add cards depending on selected size of board
+  for (let i = 0; cards.length < sizeOfDeck; i += cardOptions.length * 2) {
+    for (let j = 0; cards.length < sizeOfDeck && j < cardOptions.length; j++) {
+      cards.push(cardOptions[j], cardOptions[j]);
+    }
+  }
+  shuffle(cards);
+};
+
+var createBoard = function() {
   for (var i = 0; i < cards.length; i++) {
     var cardElement = document.createElement('img');
     cardElement.setAttribute('src', 'images/back.png');
@@ -37,17 +58,17 @@ var createBoard = function(){
     cardElement.addEventListener('click', flipCard);
     document.getElementById('game-board').appendChild(cardElement);
   }
-}
+};
 
-var checkForMatch = function(){
+var checkForMatch = function() {
   if (cardsInPlay[0] === cardsInPlay[1]) {
     matchMessage('<h3 class="match">You found a match!</h3>');
   } else {
     matchMessage('<h3 class="no-match">Sorry, try again.</h3>');
   }
-}
+};
 
-var flipCard = function(){
+var flipCard = function() {
   var cardId = this.getAttribute('data-id');
   //console.log("User flipped " + cards[cardId].rank);
   //console.log("Suit: " + cards[cardId].suit);
@@ -60,6 +81,7 @@ var flipCard = function(){
     checkForMatch();
   }
 
-}
+};
 
+generateCards(10, shuffleCards);
 createBoard();
