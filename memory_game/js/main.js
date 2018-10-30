f7d2c3a91de9c67a04bcfdf661d3d5b8600a6386
Parker Couch <parkercouch@gmail.com>
2018-10-30 15:23:34 -0700

    Lint and update js

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,3 @@
-// console.log("Up and running!");
-
 const cardOptions = [
   {
     rank: 'queen',
@@ -25,14 +23,14 @@ const cardOptions = [
 
 const cards = [];
 
-var cardsInPlay = [];
-var matchAlert = document.querySelector('.match-alert');
+const cardsInPlay = [];
+const matchAlert = document.querySelector('.match-alert');
 
-var matchMessage = function(message) {
+const matchMessage = function (message) {
   matchAlert.innerHTML = message;
 };
 
-//In place shuffle. Fisher-Yates Algorithm
+// In place shuffle. Fisher-Yates Algorithm
 const shuffleCards = (array) => {
   for (let i = array.length - 1; i > 0; i -= 1) {
     const j = Math.floor(Math.random() * (i + 1));
@@ -40,27 +38,7 @@ const shuffleCards = (array) => {
   }
 };
 
-var generateCards = function(sizeOfDeck, shuffle) {
-  // Function to add cards depending on selected size of board
-  for (let i = 0; cards.length < sizeOfDeck; i += cardOptions.length * 2) {
-    for (let j = 0; cards.length < sizeOfDeck && j < cardOptions.length; j++) {
-      cards.push(cardOptions[j], cardOptions[j]);
-    }
-  }
-  shuffle(cards);
-};
-
-var createBoard = function() {
-  for (var i = 0; i < cards.length; i++) {
-    var cardElement = document.createElement('img');
-    cardElement.setAttribute('src', 'images/back.png');
-    cardElement.setAttribute('data-id', i);
-    cardElement.addEventListener('click', flipCard);
-    document.getElementById('game-board').appendChild(cardElement);
-  }
-};
-
-var checkForMatch = function() {
+const checkForMatch = function () {
   if (cardsInPlay[0] === cardsInPlay[1]) {
     matchMessage('<h3 class="match">You found a match!</h3>');
   } else {
@@ -68,11 +46,11 @@ var checkForMatch = function() {
   }
 };
 
-var flipCard = function() {
-  var cardId = this.getAttribute('data-id');
-  //console.log("User flipped " + cards[cardId].rank);
-  //console.log("Suit: " + cards[cardId].suit);
-  //console.log("Image Path: " + cards[cardId].cardImage);
+const flipCard = function () {
+  const cardId = this.getAttribute('data-id');
+  // console.log("User flipped " + cards[cardId].rank);
+  // console.log("Suit: " + cards[cardId].suit);
+  // console.log("Image Path: " + cards[cardId].cardImage);
   cardsInPlay.push(cards[cardId].rank);
 
   this.setAttribute('src', cards[cardId].cardImage);
@@ -80,7 +58,26 @@ var flipCard = function() {
   if (cardsInPlay.length === 2) {
     checkForMatch();
   }
+};
 
+const generateCards = function (sizeOfDeck, shuffle) {
+  // Function to add cards depending on selected size of board
+  for (let i = 0; cards.length < sizeOfDeck; i += cardOptions.length * 2) {
+    for (let j = 0; cards.length < sizeOfDeck && j < cardOptions.length; j += 1) {
+      cards.push(cardOptions[j], cardOptions[j]);
+    }
+  }
+  shuffle(cards);
+};
+
+const createBoard = function () {
+  for (let i = 0; i < cards.length; i += 1) {
+    const cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+  }
 };
 
 generateCards(10, shuffleCards);
